867da98e5be45c49f021a97abf9e003d6d063fee
RinP <rinpforu@gmail.com>
2021-05-20 15:11:29 +0700

    upload note

--- lab/src/main/java/com/lab/controller/HelloController.java
+++ lab/src/main/java/com/lab/controller/HelloController.java
@@ -4,6 +4,7 @@ import com.lab.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,5 +15,10 @@ public class HelloController {
 //        model.addAttribute("myUser",user);
 //        return "hello";
         return "admin/users/index";
+    }
+
+    @RequestMapping("/test")
+    public String test(){
+        return "redirect:/hello";
     }
 }
